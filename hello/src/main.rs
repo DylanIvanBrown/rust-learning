fa0744e62bbae5d859da06dd37d1fd95ce560034
DylanIvanBrown <dylanivanbrown01@zoho.com>
2023-12-31 23:31:58 +0200

    Messing around with some basic types and conditionals

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+    let mut demo_name = "Dylan";
+
+    let demo_bool: bool = true;
+    if demo_bool {
+        println!("Demo bool is true");
+        print!("Hello {demo_name}");
+    }
+
+    demo_name = "Dylan Number 2";
+
+    println!("New demo name is {demo_name}");
+
+    println!("Min of i64 is {}", std::i64::MIN);
+    println!("Max of i64 is {}", std::i64::MAX);
+
+    println!("Max of u128 is {}", std::u128::MAX);
 }
